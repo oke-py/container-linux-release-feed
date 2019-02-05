be2fc9a2dc6407360516bc66575c30f4a5c52cc6
Naoki Oketani <okepy.naoki@gmail.com>
2019-02-05 11:23:53 +0900

    add .editorconfig

--- src/release-note.js
+++ src/release-note.js
@@ -1,22 +1,22 @@
 'use strict';
 
 module.exports = {
-    
+
   hasSecurityFixes (release_notes) {
     return release_notes.indexOf('Security fixes:') !== -1;
   },
-      
+
   extractSecurityFixes (release_notes) {
     const lines = release_notes.split('\n');
     let security_fix = [];
-  
+
     for (let i = 0; i < lines.length; i++) {
       if (! lines[i]) {
         break;
       }
       security_fix.push(lines[i]);
     }
-      
+
     return security_fix.join('\n');
-  }      
+  }
 };
