e004e1641de9f0321f7aa0a12e0cb87835cd1ca0
Naoki Oketani <okepy.naoki@gmail.com>
2019-02-12 09:38:41 +0900

    use es6 style function

--- test/release-note.test.js
+++ test/release-note.test.js
@@ -4,25 +4,25 @@
 const assert = require('assert');
 const rn = require('../src/release-note');
 
-describe('release note', function() {
-  describe('#hasSecurityFixes()', function() {
-    it('should return true for a security release', function() {
+describe('release note', () => {
+  describe('#hasSecurityFixes()', () => {
+    it('should return true for a security release', () => {
       assert.equal(rn.hasSecurityFixes('Security fixes:\n- foo\n- bar\n\nUpdates:\n- baz\n'), true);
     });
 
-    it('should return false for a non security release', function() {
+    it('should return false for a non security release', () => {
       assert.equal(rn.hasSecurityFixes('Updates:\n- baz\n'), false);
     });
   });
 
-  describe('#extractSecurityFixes()', function() {
-    it('should retrieve only security fixes', function() {
+  describe('#extractSecurityFixes()', () => {
+    it('should retrieve only security fixes', () => {
       assert.equal(rn.extractSecurityFixes('Security fixes:\n- foo\n- bar\n\nUpdates:\n- baz\n'), 'Security fixes:\n- foo\n- bar');
     });
   });
 
-  describe('#replaceLinkFormat()', function() {
-    it('should replace markdown link to slack link', function() {
+  describe('#replaceLinkFormat()', () => {
+    it('should replace markdown link to slack link', () => {
       assert.equal(rn.replaceLinkFormat('([CVE-2019-6486](https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-6486))'), '(<https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-6486|CVE-2019-6486>)');
     });
   });
