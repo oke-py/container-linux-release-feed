ece6cba27875369926765ad6204a762244f9335b
Naoki Oketani <okepy.naoki@gmail.com>
2019-03-03 12:55:59 +0900

    add test case

--- test/feed.test.js
+++ test/feed.test.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 const feed = require('../src/feed');
 
 describe('feed', () => {
-  describe('#isValidChannel', () => {
+  describe('#isValidChannel()', () => {
     it('should return true for a valid channel', () => {
       assert.equal(feed.isValidChannel('alpha'), true);
       assert.equal(feed.isValidChannel('beta'), true);
@@ -14,6 +14,17 @@ describe('feed', () => {
 
     it('should return false for a invalid channel', () => {
       assert.equal(feed.isValidChannel('test'), false);
+    });
+  });
+
+  describe('#getLatestVersion()', () => {
+    it('should return the first element of an object', () => {
+      const releases = {
+        '2023.4.0': {},
+        '1967.6.0': {},
+        '1967.5.0': {}
+      };
+      assert.equal(feed.getLatestVersion(releases), '2023.4.0');
     });
   });
 });
